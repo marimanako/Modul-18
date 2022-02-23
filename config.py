b5e781d43c843721e8ca35e0314ec08a5607e0a5
Marina <95158585+marimanako@users.noreply.github.com>
2022-02-23 14:06:57 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+TOKEN = '5198389679:AAFvUXESJ0RbcKxuUvTj5JiNzgPFc9P1dRo'
+TIMEZONE = 'Ekaterinburg'
+TIMEZONE_COMMON_NAME = 'Ekaterinburg'
+
+keys = {
+      'рубль': 'RUB',
+      'доллар': 'USD',
+      'евро': 'EUR',
+}
+
+
+
+
